bed2d068c2085647682c1f6a1bd1a86f9452e94f
Rafael Vargas <raafvargas@hotmail.com>
2019-04-04 14:48:26 -0300

    adding subscriber

--- nats/queue_subscriber.go
+++ nats/queue_subscriber.go
@@ -1,13 +1,23 @@
 package nats
 
-import nats "github.com/nats-io/go-nats"
+import (
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/myheartz/grok/logging"
+	nats "github.com/nats-io/go-nats"
+)
 
 //Subscriber is an asynchronous subscriber.
 type Subscriber struct {
 	conn     *nats.EncodedConn
 	subject  string
+	queue    string
 	handler  MessageHandler
 	messages chan *Message
+	shutdown chan os.Signal
+	done     chan bool
 }
 
 //MessageHandler handles incoming subject messages.
@@ -33,6 +43,46 @@ func (subscriber *Subscriber) SetHandler(handler MessageHandler) *Subscriber {
 	return subscriber
 }
 
-func shutdown() {
+//SetQueue sets the subscription handler.
+func (subscriber *Subscriber) SetQueue(queue string) *Subscriber {
+	subscriber.queue = queue
+	return subscriber
+}
 
+//Run starts the subject subscription.
+func (subscriber *Subscriber) Run() error {
+	_, err := subscriber.conn.BindRecvQueueChan(subscriber.subject, subscriber.queue, subscriber.messages)
+
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			subscriber.handler(
+				<-subscriber.messages,
+			)
+		}
+	}()
+
+	<-subscriber.done
+
+	return nil
+}
+
+func (subscriber *Subscriber) handleShutdown() {
+	subscriber.shutdown = make(chan os.Signal)
+	subscriber.done = make(chan bool)
+	signal.Notify(subscriber.shutdown, os.Interrupt)
+
+	go func() {
+		sig := <-subscriber.shutdown
+		logging.LogInfo("caught sig: %+v", sig)
+		logging.LogInfo("waiting 5 seconds to finish processing")
+
+		subscriber.conn.Drain()
+		subscriber.conn.FlushTimeout(5 * time.Second)
+
+		subscriber.done <- true
+	}()
 }
